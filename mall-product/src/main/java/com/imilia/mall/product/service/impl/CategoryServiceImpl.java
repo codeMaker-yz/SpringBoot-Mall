abe948ed67c4ed638dca4356e26aabfd4040a07c
codemaker <582072822@qq.com>
2021-10-29 10:53:06 +0800

    商品服务-三级分类递归查询

--- mall-product/src/main/java/com/imilia/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/imilia/mall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.imilia.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -24,6 +29,31 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        //查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        //组装成树形结构
+        List<CategoryEntity> levelMenus = entities.stream().filter(categoryEntity -> categoryEntity.getCatLevel() == 1)
+                .map(menu -> {
+                    menu.setChildren(getChildren(menu, entities));
+                    return menu;
+                }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+        return levelMenus;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> categoryEntity.getParentCid() == root.getCatId())
+                .map(categoryEntity -> {
+                    categoryEntity.setChildren(getChildren(categoryEntity, all));
+                    return categoryEntity;
+                }).sorted(Comparator.comparingInt(menu -> (menu.getSort() == null ? 0 : menu.getSort())))
+                .collect(Collectors.toList());
+        return children;
     }
 
 }
--- mall-product/src/test/java/com/imilia/mall/mallproduct/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/imilia/mall/mallproduct/MallProductApplicationTests.java
@@ -1,6 +1,5 @@
 package com.imilia.mall.mallproduct;
 
-import com.imilia.mall.product.MallProductApplication;
 import com.imilia.mall.product.entity.BrandEntity;
 import com.imilia.mall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
@@ -9,21 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest(classes = MallProductApplication.class)
+@SpringBootTest
 @RunWith(SpringRunner.class)
 class MallProductApplicationTests {
 
-    @Autowired
-    BrandService brandService;
+
 
     @Test
     void contextLoads() {
-        BrandEntity entity = new BrandEntity();
-        BrandEntity brandEntity = brandService.getById(1L);
-        System.out.println(brandEntity);
-        /*entity.setName("华为");
-        brandService.save(entity);
-        System.out.println("保存成功。。。");*/
+
     }
 
 }
